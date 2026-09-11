8bca052766cae62e94557342731a104b226ee101
kissu11 <kissu6440@gmail.com>
2026-09-11 16:52:25 +0530

    updated config file for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  agentRules: false,
-  allowedDevOrigins: ["127.0.0.1"],
+  output: 'export', // Outputs a static HTML/CSS/JS bundle to the /out folder
+  images: {
+    unoptimized: true, // GitHub Pages does not support default Next.js server-side image optimization
+  },
+  // IMPORTANT: Un-comment and configure below if your site is NOT on a custom domain
+  // basePath: '/your-repository-name', 
 };
 
-export default nextConfig;
+module.exports = nextConfig;
